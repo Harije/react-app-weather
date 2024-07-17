cb87b6d422251424815e8d2b780ca41da9ac07cb
Harije <harihariharan276@gmail.com>
2024-07-17 11:15:07 +0530

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://github.com/Harije"
+        > Hariharan</a>
+    </p>
+  </div>
+  );
+}
